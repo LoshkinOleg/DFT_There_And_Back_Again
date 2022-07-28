c1b06be8acc3bb264370fd77ecffbfad908feb98
Oleg Loshkin <loshkin.oleg.95@gmail.com>
2022-07-28 15:33:38 +0200

    Trying to figure out the origin of the clicking when FIR low pass is applied

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -1,10 +1,8 @@
-#include "DFTOutputVisualizer.h"
+#include "Filters.h"
 
 int main()
 {
-	DFTVisualizer::Application* app = new DFTVisualizer::Application(DFTVisualizer::Action::RectangularWindowLinear, "../resources/audioSamples/olegSpeech_8000Hz_32f.wav", 4.272f, 8000, 512, 720);
-	app->Run();
-	delete app;
+	Run();
 
 	return 0;
 }
